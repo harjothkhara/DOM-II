5ea32035ae76dd2f8ca9c16885a28615152ad8d7
harjothkhara <harjoth.khara@gmail.com>
2019-04-09 18:24:56 -0700

    stopPropagation

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ navLogo.addEventListener('mouseover', function(event){
 })
 
 const navAnchors = document.querySelector('.nav'); 
-// console.log(navItems);
+// console.log(navAnchors);
 navAnchors.addEventListener('dblclick', function(event){
     console.log(`navAnchors fired!`)
 })
@@ -25,6 +25,7 @@ const images = document.querySelector('img');
 // console.log(images);
 images.addEventListener('mouseover', function(event) {
     console.log(`this images event is working`)
+    event.stopPropagation();
 })
 
 window.addEventListener('scroll', function(event) {
@@ -51,6 +52,7 @@ const copyright = document.querySelector('.footer p')
 copyright.addEventListener('dblclick',function(event) {
     copyright.style.fontSize = '10rem';
     copyright.style.color = 'crimson';
+    console.log(`copyright fired!`)
 });
 
 
@@ -58,14 +60,28 @@ const p = document.querySelector('p')
 // console.log(p)
 p.addEventListener('mouseenter',function(event) {
     p.style.color = 'violet';
+    console.log(`p fired!`)
+    event.stopPropagation();
 });
 
+
+const contentPick = document.querySelector('.content-pick');
+// console.log(contentPick)
+contentPick.addEventListener('click', function(event){
+    console.log('contentPick fired!')
+});
 
 // Button
 const signMeUpBtn = document.querySelector('.btn');
-console.log(signMeUpBtn);
-
-signMeUpBtn.addEventListener('click', function(){
+// console.log(signMeUpBtn);
+signMeUpBtn.addEventListener('click', function(event){
     console.log(`signMeUpBtn fired!`)
+    event.stopPropagation();
 });
 
+const h4 = document.querySelector('h4');
+// console.log(h4);
+h4.addEventListener('click', function(event){
+    console.log(`h4 fired!`)
+    event.stopPropagation();
+});
